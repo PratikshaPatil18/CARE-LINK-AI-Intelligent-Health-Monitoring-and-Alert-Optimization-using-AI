3e18880ee980f8c76861ce5f15fc3c874c86d9f4
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2026-04-29 15:32:39 +0530

    Add files via upload

---
+++ Care_Link_BackEnd/models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['elder', 'family', 'doctor', 'admin'],
+        default: 'family'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Encrypt password using bcrypt
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
